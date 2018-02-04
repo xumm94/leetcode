5d2bd571aeeba2a0e6d31c6a710b8f0d8571c2f6
xu <xumm94@outlook.com>
2018-02-04 17:54:43 +0800

    add 115th pros

---
+++ 115_Distinct_Subsequences.cpp
@@ -0,0 +1,21 @@
+    int numDistinct(string s, string t) {
+        if(s.size() == 0)
+            return 0;
+        int m = s.size();
+        int n = t.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for(int i = 0; i <= m; i++)
+            dp[i][0] = 1;
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                dp[i][j] = dp[i - 1][j];
+                if(s[i - 1] == t[j - 1])
+                    dp[i][j] += dp[i -1][j - 1];
+            }
+        }
+        
+        return dp[m][n];
+        
+    }
